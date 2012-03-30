989bde7156e9b09dc4529581fe9202a38d16ba0d
Axel Boldt-Christmas <xmas1915@gmail.com>
2012-03-30 18:48:58 +0200

    Fixed util.IO package

--- src/game/util/IO/AcknowledgeFlag.java
+++ src/game/util/IO/AcknowledgeFlag.java
@@ -1,5 +1,15 @@
 package game.util.IO;
 
-public enum AcknowledgeFlag {
+import java.io.Serializable;
 
+public class AcknowledgeFlag implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8241511238000982199L;
+	public final AcknowledgeFlags flag;
+	public AcknowledgeFlag(AcknowledgeFlags flag) {
+		this.flag = flag;
+	}
 }
---
+++ src/game/util/IO/RequestFlags.java
@@ -0,0 +1,5 @@
+package game.util.IO;
+
+public enum RequestFlags {
+
+}
