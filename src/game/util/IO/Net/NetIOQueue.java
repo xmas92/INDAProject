faface5a1b74c2af9b9c8cb6972a0789f3c949ae
Axel Boldt-Christmas <xmas1915@gmail.com>
2012-04-02 22:00:09 +0200

    Added a synchronized IO queue

---
+++ src/game/util/IO/Net/NetIOQueue.java
@@ -0,0 +1,31 @@
+package game.util.IO.Net;
+
+import java.util.LinkedList;
+
+public class NetIOQueue {
+	private LinkedList<Package> InQueue, OutQueue;
+	private int pollCount = -1;
+	public NetIOQueue() {
+		InQueue = new LinkedList<>();
+		OutQueue = new LinkedList<>();
+	}
+	
+	public synchronized void addInPackage(Package pkg) {
+		InQueue.add(pkg);
+	}	
+	public synchronized void addOutPackage(Package pkg) {
+		OutQueue.add(pkg);
+	}
+	
+	public synchronized Package pollInPackage() {
+		if (pollCount < 0) {
+			pollCount = InQueue.size();
+			return null;
+		}
+		pollCount--;
+		return InQueue.poll();
+	}
+	public synchronized Package pollOutPackage() {
+		return OutQueue.poll();
+	}
+}
