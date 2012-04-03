482df7509292742e006cfda2362d47a16edf78d4
Axel Boldt-Christmas <xmas1915@gmail.com>
2012-04-03 16:11:12 +0200

    Fix

--- src/game/client/Map/Map.java
+++ src/game/client/Map/Map.java
@@ -1,5 +1,6 @@
 package game.client.Map;
 
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
@@ -13,16 +14,18 @@ public class Map extends TiledMap{
 	public void draw(float x, float y, int w, int h) {
 		int cTileX = (int) x / getTileWidth();
 		int cTileY = (int) y / getTileHeight();
-		int numTileX = w / getTileWidth();
-		int numTileY = h / getTileHeight();
-		x -= cTileX * getTileHeight();
-		y -= cTileY * getTileWidth();
+		int numTileX = w / getTileWidth() + 2;
+		int numTileY = h / getTileHeight() + 2;
+		x -= cTileX * getTileWidth();
+		y -= cTileY * getTileHeight();
 		for (int layer = 0; layer < getLayerCount();layer++) {
 			for (int iY = 0; iY < numTileY; iY++) {
 				if (iY + cTileY >= 0 && iY + cTileY < getHeight()) {
 					for (int iX = 0; iX < numTileX; iX++) {
 						if (iX + cTileX >= 0 && iX + cTileX  < getWidth()) {
-							getTileImage(iX + cTileX, iY + cTileY, layer).draw(iX*getTileWidth()-x, y + iY*getTileHeight()-y);
+							Image i = getTileImage(iX + cTileX, iY + cTileY, layer);
+							if (i != null)
+								i.draw(iX*getTileWidth()-x, iY*getTileHeight()-y);
 						}
 					}
 					
