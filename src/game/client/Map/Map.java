65b355dfcab04fe4e2c81f24bc777790ac03a2d3
Axel Boldt-Christmas <xmas1915@gmail.com>
2012-04-03 10:28:58 +0200

    Added prototype Map class

---
+++ src/game/client/Map/Map.java
@@ -0,0 +1,37 @@
+package game.client.Map;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Renderable;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Map extends TiledMap{
+
+	public Map(String ref) throws SlickException {
+		super(ref);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void draw(float x, float y, GameContainer gc) {
+		int w = gc.getWidth(), h = gc.getHeight();
+		int cTileX = (int) x / getTileWidth();
+		int cTileY = (int) y / getTileHeight();
+		int numTileX = w / getTileWidth();
+		int numTileY = h / getTileHeight();
+		for (int layer = 0; layer < getLayerCount();layer++) {
+			for (int iY = 0; iY < numTileY; iY++) {
+				if (iY + cTileY < 0 || iY + cTileY >= getHeight())
+					continue;
+				for (int iX = 0; iX < numTileX; iX++) {
+					if (iX + cTileX < 0 || iX + cTileX  >= getWidth())
+						continue;
+					getTileImage(iX + cTileX, iY + cTileY, layer).draw(x+ iX*getTileWidth(), y + iY*getTileHeight());
+				}
+				
+			}
+		}
+	}
+	
+	
+	
+}
